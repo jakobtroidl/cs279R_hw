27ce5f493cce77e6d3dd0705dc8ab5dc3b9e194b
Jakob Troidl <jakob.troidl@googlemail.com>
2022-09-09 00:30:30 -0400

    update

--- hw1/controllers/settings.js
+++ hw1/controllers/settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-    port: '3001',
+    port: '3002',
     // Connect to MonggoDb, 
     // example atlas : mongodb+srv://zekaistodo:password@clusters.rqvdo.mongodb.net/Database?retryWrites=true&w=majority
     // example local : mongodb://127.0.0.1:27017
-    dbURI: "mongodb+srv://jakobtroidl:uBedQcxSylQsbWoC@cs279r-hw1-mongodb-v1.bpjw2at.mongodb.net/?retryWrites=true&w=majority"
+    dbURI: "mongodb+srv://jakobtroidl:cs279risfun@cs279r-hw1-mongodb-v1.bpjw2at.mongodb.net/?retryWrites=true&w=majority"
 }
